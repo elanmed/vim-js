daf0b9c6f9b5129820ed1fd9fe0d102179b34126
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2026-01-22 23:44:04 -0500

    feat: support two key keymaps

--- scripts/content.js
+++ scripts/content.js
@@ -1,8 +1,33 @@
+let firstKey = null;
+
+const twoKeyKeymaps = ["gg", "yy"];
+
 document.addEventListener("keydown", (event) => {
   if (event.target.tagName === "INPUT" || event.target.tagName === "TEXTAREA") {
     return;
   }
 
+  if (firstKey === null) {
+    const isFirstKeyOfKeymap = twoKeyKeymaps.some((keymap) =>
+      keymap.startsWith(event.key),
+    );
+    if (isFirstKeyOfKeymap) {
+      // TODO: set a timeout?
+      firstKey = event.key;
+      return;
+    }
+
+    handleSingleKeyKeymap(event);
+  } else {
+    handleTwoKeyKeymap(event, firstKey);
+    firstKey = null;
+  }
+});
+
+/**
+ * @param {KeyboardEvent} event
+ */
+function handleSingleKeyKeymap(event) {
   switch (event.key) {
     case "j": {
       window.scrollBy({
@@ -18,5 +43,28 @@ document.addEventListener("keydown", (event) => {
       });
       break;
     }
+    case "G": {
+      window.scrollBy({
+        behavior: "instant",
+        top: document.documentElement.scrollHeight,
+      });
+      break;
+    }
   }
-});
+}
+
+/**
+ * @param {KeyboardEvent} event
+ * @param {string} firstKey
+ */
+function handleTwoKeyKeymap(event, firstKey) {
+  switch (firstKey.concat(event.key)) {
+    case "gg": {
+      window.scrollBy({
+        behavior: "instant",
+        top: -document.documentElement.scrollHeight,
+      });
+      break;
+    }
+  }
+}
