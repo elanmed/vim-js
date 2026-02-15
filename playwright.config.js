8f1f2b050d5888f32caa304776a4f838c0bf2600
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2026-02-15 16:01:23 -0500

    tests: simplify playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -1,17 +1,12 @@
 const { defineConfig, devices } = require('@playwright/test');
-const path = require('path');
 
 module.exports = defineConfig({
   testDir: './tests',
   fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
     trace: 'on-first-retry',
   },
-
   projects: [
     {
       name: 'chromium',
